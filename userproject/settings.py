1d5101d5664dd0ec779baace06b26435990f3a5e
219ashishyadav <107169476+219ashishyadav@users.noreply.github.com>
2023-06-25 16:52:45 +0530

    Add files via upload

--- userproject/settings.py
+++ userproject/settings.py
@@ -75,13 +75,11 @@ WSGI_APPLICATION = 'userproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+ DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -122,6 +120,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,"static")
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
